7adbb79a4477e3335da5a546edef47c2503147c2
austinlee <austinlee@192.168.1.9>
2018-12-19 17:18:31 -0600

    Implemented an OBJLoader class to load a model from an .obj file

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TexturedModels;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shader.StaticShader;
 import textures.ModelTexture;
@@ -44,96 +45,98 @@ public class MainGameLoop {
 //        		1, 0	//V3
 //        };
         
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
+//		float[] vertices = {			
+//				-0.5f,0.5f,-0.5f,	
+//				-0.5f,-0.5f,-0.5f,	
+//				0.5f,-0.5f,-0.5f,	
+//				0.5f,0.5f,-0.5f,		
+//				
+//				-0.5f,0.5f,0.5f,	
+//				-0.5f,-0.5f,0.5f,	
+//				0.5f,-0.5f,0.5f,	
+//				0.5f,0.5f,0.5f,
+//				
+//				0.5f,0.5f,-0.5f,	
+//				0.5f,-0.5f,-0.5f,	
+//				0.5f,-0.5f,0.5f,	
+//				0.5f,0.5f,0.5f,
+//				
+//				-0.5f,0.5f,-0.5f,	
+//				-0.5f,-0.5f,-0.5f,	
+//				-0.5f,-0.5f,0.5f,	
+//				-0.5f,0.5f,0.5f,
+//				
+//				-0.5f,0.5f,0.5f,
+//				-0.5f,0.5f,-0.5f,
+//				0.5f,0.5f,-0.5f,
+//				0.5f,0.5f,0.5f,
+//				
+//				-0.5f,-0.5f,0.5f,
+//				-0.5f,-0.5f,-0.5f,
+//				0.5f,-0.5f,-0.5f,
+//				0.5f,-0.5f,0.5f
+//				
+//		};
+//		
+//		float[] textureCoords = {
+//				
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,			
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,			
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0
+//
+//				
+//		};
+//		
+//		int[] indices = {
+//				0,1,3,	
+//				3,1,2,	
+//				4,5,7,
+//				7,5,6,
+//				8,9,11,
+//				11,9,10,
+//				12,13,15,
+//				15,13,14,	
+//				16,17,19,
+//				19,17,18,
+//				20,21,23,
+//				23,21,22
+//
+//		};
+        RawModel model = OBJLoader.loadObjModel("stall", loader);
         //Update
-        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-        ModelTexture modelTexture = new ModelTexture(loader.loadTexture("elephant"));
+//        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        ModelTexture modelTexture = new ModelTexture(loader.loadTexture("stallTexture"));
         TexturedModels texturedModel = new TexturedModels(model, modelTexture);
-        Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1f);
+        Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -50), 0, 0, 0, 1f);
+        
         while(!Display.isCloseRequested()){
         	camera.move();
         	
 //        	entity.increasePosition(0, 0, -.002f);
 //        	entity.increaseRotation(0, 0, 0);
             //game logic
-        	entity.increaseRotation(1, 1, 0);
+        	entity.increaseRotation(0, 0.9f, 0);
             renderer.prepare();
             shader.start();
             shader.loadViewMatrix(camera);
